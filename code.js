caa33496f1c1978b36fcc6b40c2f0a2d52d001f5
Joel Chambers <104284414+Boomboomyoyo@users.noreply.github.com>
2023-10-24 19:55:38 -0600

    Partial solution, will come back when fresh

--- code.js
+++ code.js
@@ -1,3 +1,33 @@
-function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+function depthFirstSearch(graph, startNode, targetNode, visitedNodes, path) {
+    if(startNode == targetNode)
+    {
+        return true
+    }
+    for(i = 0; i < graph[startNode].length; console.log(i++))
+    {
+        if(graph[startNode][i] && visitedNodes[graph[startNode][i]] != true)
+        {
+            visitedNodes[graph[startNode][i]] = true
+            path.push(graph[startNode][i])
+            deadEnd = depthFirstSearch(graph, graph[startNode][i], targetNode, visitedNodes, path)
+            if(deadEnd)
+            {
+                return deadEnd
+            }
+        } else
+        {
+            path.pop()
+        }
+    }
+    console.log(false)
+    return false
 }
+
+
+g = [[1,2],[1],[0,1]]
+target = 2
+start = 0
+visitedNodes = []
+path = [start]
+depthFirstSearch(g, start, target, visitedNodes, path)
+console.log(path)
---
+++ code.test.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const jsc = require('jsverify');
+
+eval(fs.readFileSync('code.js')+'');
+
+const test =
+    jsc.forall("array (pair nat nat)", function(edges) {
+        var max = edges.reduce(function(a, b) { return Math.max(a, Math.max(b[0], b[1])); }, 0);
+        var mat = [];
+        for(var i = 0; i <= max; i++) {
+            mat[i] = [];
+            for(var j = 0; j <= max; j++) {
+                mat[i][j] = 0;
+            }
+            for(var j = 0; j < edges.length; j++) {
+                if(edges[j][0] == i) mat[i][edges[j][1]] = 1;
+            }
+        }
+        var list = [];
+        for(var i = 0; i <= max; i++) {
+            list[i] = [];
+            for(var j = 0; j < edges.length; j++) {
+                if(edges[j][0] == i) list[i].push(edges[j][1]);
+            }
+            list[i].sort(function(a, b) { return a - b; });
+            list[i] = [...new Set(list[i])];
+        }
+        return console.log(list);
+    });
+jsc.assert(test, { tests: 1000 });
